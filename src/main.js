a175aafa1508ca201b81b7d512071cf0be266c80
Abygail27 <abygaailrrocha@gmail.com>
2019-03-09 11:43:21 -0600

    creando tres htmls para pagina de inicio, registro y login

---
+++ src/main.js
@@ -0,0 +1,16 @@
+const buttonForLoggingIn = document.getElementById('registration');
+
+
+
+
+buttonForLoggingIn.addEventListener ('click', () => {
+
+let email = document.getElementById('email').value; 
+let passcode = document.getElementById('passcode').value; 
+
+//console.log('you clicked me');
+console.log(email);
+console.log(passcode);
+});
+
+
