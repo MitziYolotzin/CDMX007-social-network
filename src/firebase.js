c539db687357ee98e7e8535a5ea6585ccb6be61f
MitziYolotzin <ultravisuell@hotmail.com>
2019-03-20 19:47:11 -0600

    login y muro uniendo pruebas

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+      const config = {
+        apiKey: "AIzaSyDOvN36a9Ec3svVs5XpAwUF0gd9wBaVy7c",
+        authDomain: "social-network-f8e7a.firebaseapp.com",
+        databaseURL: "https://social-network-f8e7a.firebaseio.com",
+        projectId: "social-network-f8e7a",
+        storageBucket: "social-network-f8e7a.appspot.com",
+        messagingSenderId: "454382512914"
+      };
+      firebase.initializeApp(config);
